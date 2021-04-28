c9e04f437f1601e6750cc653041492553dd7b2b3
Bombohog <70882697+Bombohog@users.noreply.github.com>
2021-04-28 11:00:12 +0200

    Initial commit

---
+++ app/src/main/java/dk/awayoflife/androiddevfun_3/SimpleAsyncTask.java
@@ -0,0 +1,59 @@
+/********************************************
+ * Project description
+ *
+ * Created by: Lasse J. Kongsdal
+ * Date: 28-04-2021
+ *
+ * Description of program
+ ********************************************/
+
+package dk.awayoflife.androiddevfun_3;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private WeakReference<TextView> mTextView;
+
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<>(tv);
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running
+        int s = n * 200;
+
+        // Sleep for the random amount of time
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+
+}
